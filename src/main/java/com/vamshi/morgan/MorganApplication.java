4156a6b6a4dfd4be255fbf7eb404e24b9e753123
11vamshi <vamshi.patha.11@gmail.com>
2022-12-21 19:15:51 -0500

    CPPNAVIG-1004 first commit

---
+++ src/main/java/com/vamshi/morgan/MorganApplication.java
@@ -0,0 +1,13 @@
+package com.vamshi.morgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vamshi/morgan/MorganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamshi.morgan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
